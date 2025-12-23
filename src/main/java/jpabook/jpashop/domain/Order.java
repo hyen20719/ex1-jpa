2dfe0cae3accf7ebb6d66bc05c3e44772cfb480b
hyen <hyen20719@gmail.com>
2025-12-23 19:19:38 +0900

    [jpa1]- 요구 사항 분석과 기본 매핑

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING) // ordinal은 오류발생할 수 있으니 string으로
+    private Orderstatus orderstatus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Orderstatus getOrderstatus() {
+        return orderstatus;
+    }
+
+    public void setOrderstatus(Orderstatus orderstatus) {
+        this.orderstatus = orderstatus;
+    }
+}
